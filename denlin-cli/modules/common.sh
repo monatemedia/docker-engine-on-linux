e0a988f757be28b00b4acb336e508496b77fb95d
Edward Baitsewe <edward@monatemedia.com>
2025-01-09 23:13:05 +0200

    bug fix

--- denlin-cli/modules/common.sh
+++ denlin-cli/modules/common.sh
@@ -6,24 +6,6 @@
 # Resolve the project root directory
 PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
-# ASCII Art Banner
-display_banner() {
-    echo -e "                                                                 "
-    echo -e "         _____     ______     __   __     __         __     __   __   "
-    echo -e "        /\\  __-.  /\\  ___\\   /\\ \"-.\\ \\   /\\ \\       /\\ \\   /\\ \"-.\\ \\  "
-    echo -e "        \\ \\ \\/\\ \\ \\ \\  __\\   \\ \\ \\-.  \\  \\ \\ \\____  \\ \\ \\  \\ \\ \\-.  \\ "
-    echo -e "         \\ \\____-  \\ \\_____\\  \\ \\_ \\\"\\_\\  \\ \\_____\\  \\ \\_\\  \\ \\_\\ \"\\_\\"
-    echo -e "          \\/____/   \\/_____/   \\/_/ \\/_/   \\/_____/   \\/_/   \\/_/ \\/_/"
-    echo -e ""
-    echo -e "   ______     ______     ______     __   __   __     ______     ______     ______    "
-    echo -e "  /\  ___\   /\  ___\   /\  == \   /\ \ / /  /\ \   /\  ___\   /\  ___\   /\  ___\   "
-    echo -e "  \ \___  \  \ \  __\   \ \  __<   \ \ \'/   \ \ \  \ \ \____  \ \  __\   \ \___  \  "
-    echo -e "   \/\_____\  \ \_____\  \ \_\ \_\  \ \__|    \ \_\  \ \_____\  \ \_____\  \/\_____\ "
-    echo -e "    \/_____/   \/_____/   \/_/ /_/   \/_/      \/_/   \/_____/   \/_____/   \/_____/ "
-    echo -e "                                                                 "                  
-    echo -e "                                                                 "
-}
-
 # Start the services menu
 start_services() {
     SERVICES_SCRIPT="$PROJECT_ROOT/services/services.sh"
@@ -36,5 +18,4 @@ start_services() {
 }
 
 # Main logic
-display_banner
 start_services
