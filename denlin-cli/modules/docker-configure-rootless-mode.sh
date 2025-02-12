73c4cf4f4c1347309ac8009790036d8e6797f940
Edward Baitsewe <edward@monatemedia.com>
2025-02-12 03:13:40 +0200

    update rootless script

--- denlin-cli/modules/docker-configure-rootless-mode.sh
+++ denlin-cli/modules/docker-configure-rootless-mode.sh
@@ -17,7 +17,13 @@ sudo rm -f /var/run/docker.sock
 echo "Installing Docker in Rootless Mode..."
 dockerd-rootless-setuptool.sh install
 
+echo "Starting Docker Rootless Daemon..."
+PATH=/usr/bin:/sbin:/usr/sbin:$PATH dockerd-rootless.sh &
+
+sleep 5  # Give the daemon some time to start
+
 echo "Verifying Docker Rootless Mode..."
-docker run hello-world
+docker run hello-world || echo "Failed to connect to Docker daemon. Try running: PATH=/usr/bin:/sbin:/usr/sbin:\$PATH dockerd-rootless.sh"
 
 echo "Docker Rootless Mode installed successfully!"
+
