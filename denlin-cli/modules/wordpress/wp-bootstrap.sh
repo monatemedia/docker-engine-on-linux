d4b7fef56cbf85b881882ff164a8422c7a205c69
Edward Baitsewe <edward@monatemedia.com>
2025-05-25 09:24:32 +0200

    Update wp-bootstrap.wp to install WordPress core

--- denlin-cli/modules/wordpress/wp-bootstrap.sh
+++ denlin-cli/modules/wordpress/wp-bootstrap.sh
@@ -20,6 +20,16 @@ docker exec -it "$CONTAINER_NAME" bash -c "
     command wp "$@" --allow-root
   }
 
+  # Install WordPress core if not installed
+  if ! wp core is-installed; then
+    wp core install \
+      --url="$WP_SITE_URL" \
+      --title="$WP_SITE_TITLE" \
+      --admin_user="$WP_ADMIN_USER" \
+      --admin_password="$WP_ADMIN_PASS" \
+      --admin_email="$WP_ADMIN_EMAIL"
+  fi
+
   # Update WordPress core
   wp core update
 
